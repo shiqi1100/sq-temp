df39ffedfb232388d067078174dde4412fcf5fb7
ZhangShiQi <q18056090106@163.com>
2023-09-13 17:07:19 +0800

    feat: 🎸 center

--- src/views/MyView.vue
+++ src/views/MyView.vue
@@ -6,14 +6,27 @@
     <div>2</div>
     <div>3</div>
   </div>
+  <div>
+    <div class="center">
+      <div class="item">555</div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped>
+<style scoped lang="scss">
 .px-box {
   border: 1px solid red;
   width: 375px;
   height: 200px;
+}
+
+.center {
+  display: grid;
+  border: 1px solid gray;
+  width: 100px;
+  height: 100px;
+  place-items: center;
 }
 </style>
