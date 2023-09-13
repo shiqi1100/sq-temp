5dae40fe7bedd3f1bd09bed39c9afd6fd5bf804b
ZhangShiQi <q18056090106@163.com>
2023-09-13 16:45:57 +0800

    feat: 🎸 postcss-html/scss

---
+++ src/views/MyView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="px-box"></div>
+
+  <div>
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.px-box {
+  border: 1px solid red;
+  width: 375px;
+  height: 200px;
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import autoprefixer from 'autoprefixer'
+import postCssPxToRem from 'postcss-pxtorem'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueJsx()],
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({
+          overrideBrowserslist: ['Android 4.1', 'iOS 7.1', 'Chrome > 31', 'ff > 31', 'ie >= 8']
+        }),
+        postCssPxToRem({
+          // 自适应，px>rem转换
+          rootValue: 37.5, // 75表示750设计稿，37.5表示375设计稿
+          propList: ['*'], // 需要转换的属性，这里选择全部都进行转换
+          selectorBlackList: ['.van'], // 过滤掉不进行rem转换
+          exclude: '/node_modules' // 忽略包文件转换rem
+        })
+      ]
+    }
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
+//https://github.com/cuth/postcss-pxtorem
