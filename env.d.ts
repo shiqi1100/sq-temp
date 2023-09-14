99564afbfd634294ad8bf0efd8873122f9b58556
ZhangShiQi <q18056090106@163.com>
2023-09-14 16:46:40 +0800

    test: 💍 test

--- env.d.ts
+++ env.d.ts
@@ -1,11 +1,5 @@
 // / <reference types="vite/client" />
-// import type {Plugin_2} from 'vite'
-//
-// type ESLintPlugin = {
-//     include: string[]
-// }
-//
-// declare module 'vite-plugin-eslint' {
-//     const VitePluginESLint: (arg: ESLintPlugin) => Plugin_2
-//     export default VitePluginESLint
-// }
+declare module '*.vue' {
+  import Vue from 'vue'
+  export default Vue
+}
