04226f8877285bb570ac63c08e29fc4dfe647867
ZhangShiQi <q18056090106@163.com>
2023-09-14 16:18:35 +0800

    vite change

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,11 @@
 // / <reference types="vite/client" />
+// import type {Plugin_2} from 'vite'
+//
+// type ESLintPlugin = {
+//     include: string[]
+// }
+//
+// declare module 'vite-plugin-eslint' {
+//     const VitePluginESLint: (arg: ESLintPlugin) => Plugin_2
+//     export default VitePluginESLint
+// }
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import autoprefixer from 'autoprefixer'
-import postCssPxToRem from 'postcss-pxtorem'
+// import postCssPxToRem from 'postcss-pxtorem'
 import eslintPlugin from 'vite-plugin-eslint'
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,14 +20,14 @@ export default defineConfig({
       plugins: [
         autoprefixer({
           overrideBrowserslist: ['Android 4.1', 'iOS 7.1', 'Chrome > 31', 'ff > 31', 'ie >= 8']
-        }),
-        postCssPxToRem({
-          // 自适应，px>rem转换
-          rootValue: 37.5, // 75表示750设计稿，37.5表示375设计稿
-          propList: ['*'], // 需要转换的属性，这里选择全部都进行转换
-          selectorBlackList: ['.van'], // 过滤掉不进行rem转换
-          exclude: '/node_modules' // 忽略包文件转换rem
         })
+        // postCssPxToRem({
+        //   // 自适应，px>rem转换
+        //   rootValue: 37.5, // 75表示750设计稿，37.5表示375设计稿
+        //   propList: ['*'], // 需要转换的属性，这里选择全部都进行转换
+        //   selectorBlackList: ['.van'], // 过滤掉不进行rem转换
+        //   exclude: '/node_modules' // 忽略包文件转换rem
+        // })
       ]
     }
   },
