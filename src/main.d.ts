921f2b7c7f9e4d084ecdffe7a791b27676ca79a3
ZhangShiQi <q18056090106@163.com>
2023-09-14 15:57:35 +0800

    feat: 🎸 remove nouse

--- src/main.d.ts
+++
@@ -1,2 +0,0 @@
-import './assets/main.css'
-import 'amfe-flexible'
--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-function add(a: any, b) {
-  console.log('add', a, b)
-}
-add(1, 2)
+
 const app = createApp(App)
 
 app.use(createPinia())
