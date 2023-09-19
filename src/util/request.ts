513bff35d5a0df91ddb537321c3b42f49a904a20
ZhangShiQi <q18056090106@163.com>
2023-09-19 09:03:08 +0800

    perf: ⚡️ 解决报错

--- src/util/request.ts
+++ src/util/request.ts
@@ -17,11 +17,11 @@ class AxiosHttp {
   initInterceptors() {
     // 添加请求拦截器
     this.instance.interceptors.request.use(
-      function (config) {
+      function(config) {
         // 在发送请求之前做些什么
         return config
       },
-      function (error) {
+      function(error) {
         // 对请求错误做些什么
         return Promise.reject(error)
       }
@@ -29,12 +29,12 @@ class AxiosHttp {
 
     // 添加响应拦截器
     this.instance.interceptors.response.use(
-      function (response) {
+      function(response) {
         // 2xx 范围内的状态码都会触发该函数。
         // 对响应数据做点什么
         return response
       },
-      function (error) {
+      function(error) {
         // 超出 2xx 范围的状态码都会触发该函数。
         // 对响应错误做点什么
         return Promise.reject(error)
