118231c8f5f5ad9daa2e5d1a77f2467d29a93607
ZhangShiQi <q18056090106@163.com>
2023-09-20 10:25:16 +0800

    feat: 🎸 添加pinia持续化

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,13 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
--- src/stores/userInfo.ts
+++ src/stores/userInfo.ts
@@ -1,10 +1,16 @@
-export const userInfoStore = defineStore('main', () => {
-  const useInfoData = reactive({
-    userInfo: {},
-    token: '',
-    companyId: '',
-    profileId: '',
-    realName: ''
-  })
-  return { useInfoData }
-})
+export const userInfoStore = defineStore(
+  'main',
+  () => {
+    const useInfoData = reactive({
+      userInfo: {},
+      token: '',
+      companyId: '',
+      profileId: '',
+      realName: ''
+    })
+    return { useInfoData }
+  },
+  {
+    persist: true
+  }
+)
